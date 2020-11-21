330cd5d20bb292218454c91136d038743ece91a9
Ali Asgar Zainal Abidin <arogan929@gmail.com>
2020-11-21 21:48:52 +0800

    Perbaiki tampilan item

--- app/src/main/java/com/selayar/history/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/selayar/history/fragment/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.selayar.history.fragment.home
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
@@ -10,26 +11,35 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
 import com.selayar.history.BuildConfig.TAG
 import com.selayar.history.R
 import com.selayar.history.Models.WisataSejarah
 import com.selayar.history.Util.ExampleData
 import com.selayar.history.di.Injectable
+import io.realm.Realm
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.rv_layout_item.*
 import kotlinx.android.synthetic.main.rv_layout_item.view.*
 import kotlinx.android.synthetic.main.rv_layout_item.view.iv_layout_item_bg
+import java.util.jar.Manifest
+import javax.inject.Inject
 
 class HomeFragment : Fragment(), Injectable, WisataOnClickListener {
 
     var listWisata = ArrayList<WisataSejarah>()
+
+    @Inject
+    lateinit var realm: Realm
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +51,19 @@ class HomeFragment : Fragment(), Injectable, WisataOnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                fab_home_scan.isEnabled = true
+            }
+            else -> {
+                fab_home_scan.isEnabled = false
+            }
+        }
 
         val snapHelper = GravitySnapHelper(Gravity.CENTER)
         snapHelper.attachToRecyclerView(rv_home_item)
@@ -62,6 +85,11 @@ class HomeFragment : Fragment(), Injectable, WisataOnClickListener {
                 true
             }
         }
+
+        fab_home_scan.setOnClickListener {
+            (activity as AppCompatActivity).findNavController(R.id.nav_host_fragment_container)
+                .navigate(R.id.action_homeFragment_to_scannerFragment)
+        }
     }
 
     override fun WisataOnClickListener(
@@ -76,6 +104,6 @@ class HomeFragment : Fragment(), Injectable, WisataOnClickListener {
             root.tv_layout_item_ket to "ket"
         )
         (activity as AppCompatActivity).findNavController(R.id.nav_host_fragment_container)
-            .navigate(R.id.action_homeFragment_to_detailFragment,null,null, extras)
+            .navigate(R.id.action_homeFragment_to_detailFragment, null, null, extras)
     }
 }
---
+++ app/src/main/java/com/selayar/history/fragment/scan/ScannerFragment.kt
@@ -0,0 +1,62 @@
+package com.selayar.history.fragment.scan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.selayar.history.R
+import kotlinx.android.synthetic.main.fragment_scanner.*
+
+class ScannerFragment : Fragment() {
+
+    lateinit var codeScanner: CodeScanner
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scanner, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        codeScanner = CodeScanner(requireActivity(), scanner)
+
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        codeScanner.decodeCallback = DecodeCallback {
+            activity?.runOnUiThread {
+                Toast.makeText(context, it.text, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        scanner.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+}
