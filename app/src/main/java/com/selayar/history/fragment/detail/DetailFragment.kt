7c58e669e5e157f22a0d09adc54c45c26bfc2374
Ali Asgar Zainal Abidin <arogan929@gmail.com>
2020-11-22 05:36:04 +0800

    Tambah API getQrCode

--- app/src/main/java/com/selayar/history/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/selayar/history/fragment/detail/DetailFragment.kt
@@ -8,11 +8,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.selayar.history.Featured.GlideApp
+import com.selayar.history.Model.ModelWrapper
+import com.selayar.history.Models.WisataSejarah
 import com.selayar.history.R
+import com.selayar.history.Retrofit.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_detail.*
 import java.util.concurrent.TimeUnit
 
 class DetailFragment : Fragment() {
+
+    companion object{
+        val SLUG = "SLUG"
+        val NAMA = "NAMA"
+        val DESCRIPTION = "DESCRIPTION"
+        val LOCATION = "LOCATION"
+        val BG = "BG"
+    }
+
+    private val restForeground by lazy { ApiFactory.create(false) }
+    private var disposable: Disposable? = null
+    private lateinit var nama : String
+    private lateinit var description : String
+    private lateinit var location : String
+    private lateinit var bg : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +53,39 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        nama = arguments?.getString(NAMA).toString()
+        description = arguments?.getString(DESCRIPTION).toString()
+        location = arguments?.getString(LOCATION).toString()
+        bg = arguments?.getString(BG).toString()
+
+        Glide.with(this)
+            .load(bg)
+            .into(iv_detail_bg)
+
+        tv_detail_title.setText(nama)
+        tv_detail_ket.setText(description)
+
         tv_detail_more.setOnClickListener {
             findNavController().navigate(R.id.action_detailFragment_to_moreFragment)
         }
     }
+
+//    fun getQRCode() {
+//        disposable = restForeground
+//            .getQR()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//
+//            }, {
+//
+//            })
+//    }
+//
+//    fun onSuccessQR(data: ModelWrapper<WisataSejarah>) {
+//        data.data?.let {
+//
+//        }
+//    }
 
 }
--- app/src/main/java/com/selayar/history/fragment/scan/ScannerFragment.kt
+++ app/src/main/java/com/selayar/history/fragment/scan/ScannerFragment.kt
@@ -1,21 +1,36 @@
 package com.selayar.history.fragment.scan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ScanMode
+import com.selayar.history.BuildConfig.TAG
+import com.selayar.history.Model.ModelListWrapper
+import com.selayar.history.Model.ModelWrapper
+import com.selayar.history.Models.WisataSejarah
 import com.selayar.history.R
+import com.selayar.history.Retrofit.ApiFactory
+import com.selayar.history.fragment.detail.DetailFragment
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers.io
 import kotlinx.android.synthetic.main.fragment_scanner.*
 
 class ScannerFragment : Fragment() {
 
     lateinit var codeScanner: CodeScanner
+
+    private val restForeground by lazy { ApiFactory.create(false) }
+    private var disposable: Disposable? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +55,8 @@ class ScannerFragment : Fragment() {
 
         codeScanner.decodeCallback = DecodeCallback {
             activity?.runOnUiThread {
-                Toast.makeText(context, it.text, Toast.LENGTH_SHORT)
-                    .show()
+                getQRCode(it.toString())
+                Toast.makeText(context, it.toString(), Toast.LENGTH_SHORT).show()
             }
         }
         scanner.setOnClickListener {
@@ -57,6 +72,42 @@ class ScannerFragment : Fragment() {
     override fun onPause() {
         codeScanner.releaseResources()
         super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
+    fun getQRCode(slug : String) {
+        disposable = restForeground
+            .getQR(slug)
+            .subscribeOn(io())
+            .observeOn(mainThread())
+            .subscribe({
+                codeScanner.releaseResources()
+                onSuccessQR(it)
+            }, {
+                Toast.makeText(context, it.toString(), Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "getQRCode: ${it.toString()}")
+            })
+    }
+
+    fun onSuccessQR(data: ModelListWrapper<WisataSejarah>) {
+        data.data?.let {
+            val bundle = Bundle()
+            for (data in it){
+                bundle.putString(DetailFragment.NAMA,data.nama)
+                bundle.putString(DetailFragment.DESCRIPTION,data.deskripsi)
+                bundle.putString(DetailFragment.BG,data.bg)
+                bundle.putString(DetailFragment.LOCATION,data.location)
+
+                Toast.makeText(context, data.nama, Toast.LENGTH_SHORT).show()
+            }
+
+            (activity as AppCompatActivity).findNavController(R.id.nav_host_fragment_container)
+                .navigate(R.id.action_scannerFragment_to_detailFragment, bundle)
+        }
     }
 
 }
